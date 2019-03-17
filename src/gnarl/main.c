6b743f26774bd38363fed07abf526bc8b9193b12
Eric Cooper <ecc@cmu.edu>
2019-03-17 18:33:12 -0400

    Initialize display before BLE task to avoid panic

--- src/gnarl/main.c
+++ src/gnarl/main.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include <esp_wifi.h>
+
 #include "display.h"
 #include "gnarl.h"
 #include "rfm95.h"
@@ -8,13 +10,12 @@
 #define PUMP_FREQUENCY 916600000
 
 void app_main() {
+	esp_wifi_stop();
 	rfm95_init();
 	uint8_t v = read_version();
 	ESP_LOGD(TAG, "radio version %d.%d", version_major(v), version_minor(v));
 	set_frequency(PUMP_FREQUENCY);
 	ESP_LOGD(TAG, "frequency set to %d Hz", read_frequency());
-
-	gnarl_init();
-
 	display_init();
+	gnarl_init();
 }
