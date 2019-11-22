68c03b79c766537c9f286097094d7688cfa2c14c
d3xr <xr.name@gmail.com>
2019-11-22 09:43:08 +0300

    Add src folder

---
+++ src/pickle/pickle.h
@@ -0,0 +1,25 @@
+#include <stdint.h>
+
+#include "version.h"
+
+#define LOG_LOCAL_LEVEL	ESP_LOG_DEBUG
+#include <esp_log.h>
+
+#define MILLISECONDS	1000
+#define SECONDS		1000000
+
+#define MHz		1000000
+
+#define TAG "pickle"
+
+// Track github.com/ps2/rileylink/firmware/ble113_rfspy/gatt.xml
+#define BLE_RFSPY_VERSION  "ble_rfspy 2.0"
+
+#define STATE_OK "OK"
+
+void pickle_init(void);
+void start_pickle_task(void);
+void rfspy_command(const uint8_t *buf, int count, int rssi);
+void send_code(const uint8_t code);
+void send_bytes(const uint8_t *buf, int count);
+void print_bytes(const char* msg, const uint8_t *buf, int count);
