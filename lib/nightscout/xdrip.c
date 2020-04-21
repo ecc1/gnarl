2852e284d0efc516c457fc633958fbcb8bc2870b
Eric Cooper <ecc@cmu.edu>
2020-04-21 14:29:24 -0400

    Handle choice of xDrip host based on network configuration

--- lib/nightscout/xdrip.c
+++ lib/nightscout/xdrip.c
@@ -1,15 +1,27 @@
+#define TAG		"NS"
+#define LOG_LOCAL_LEVEL	ESP_LOG_DEBUG
+#include <esp_log.h>
+
+#include "network.h"
 #include "nightscout.h"
 #include "nightscout_config.h"
 
 esp_err_t http_header_callback(esp_http_client_event_t *e);
 
-esp_http_client_handle_t xdrip_client_handle(const char *hostname) {
+#ifdef USE_BLUETOOTH_TETHERING
+#define XDRIP_HOST	gateway_address()
+#else
+#define XDRIP_HOST	XDRIP_WIFI_HOST
+#endif
+
+esp_http_client_handle_t xdrip_client_handle(void) {
 	static char url[256];
-	snprintf(url, sizeof(url), "http://%s:17580/sgv.json", hostname);
+	snprintf(url, sizeof(url), "http://%s:17580/sgv.json", XDRIP_HOST);
 	esp_http_client_config_t config = {
 		.url = url,
 		.event_handler = http_header_callback,
 	};
+	ESP_LOGI(TAG, "xDrip URL: %s", config.url);
 	esp_http_client_handle_t client = esp_http_client_init(&config);
 	esp_http_client_set_header(client, "api-secret", NIGHTSCOUT_API_SECRET);
 	return client;
