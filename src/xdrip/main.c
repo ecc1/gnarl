e76747968434189ee3a6f7de837128277abec3a6
Eric Cooper <ecc@cmu.edu>
2020-04-21 14:30:10 -0400

    Add xdrip application

---
+++ src/xdrip/main.c
@@ -0,0 +1,16 @@
+#include <time.h>
+
+#include "network.h"
+#include "nightscout.h"
+#include "timezone_config.h"
+
+void app_main(void) {
+	printf("IP address: %s\n", ip_address());
+	setenv("TZ", TZ, 1);
+	tzset();
+	char *response = http_get(xdrip_client_handle());
+	if (http_server_time) {
+		printf("%s  server time\n", time_string(&http_server_time));
+	}
+	process_nightscout_entries(response, print_nightscout_entry);
+}
