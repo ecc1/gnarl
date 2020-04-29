cbbd58c43b601f9746611d3d1066c9308967ba25
Eric Cooper <ecc@cmu.edu>
2020-04-28 20:03:59 -0400

    Add ESP_LOG* macros that print to papertrail

--- lib/papertrail/papertrail.h
+++ lib/papertrail/papertrail.h
@@ -7,4 +7,18 @@ extern FILE *papertrail;
 
 int papertrail_init(void);
 
+#undef ESP_LOGE
+#define ESP_LOGE(tag, fmt, ...)							\
+	do {									\
+		fprintf(stderr, fmt "\n", ##__VA_ARGS__);			\
+		if (papertrail) fprintf(papertrail, fmt "\n", ##__VA_ARGS__);	\
+	} while (0)
+
+#undef ESP_LOGI
+#define ESP_LOGI(tag, fmt, ...)							\
+	do {									\
+		fprintf(stderr, fmt "\n", ##__VA_ARGS__);			\
+		if (papertrail) fprintf(papertrail, fmt "\n", ##__VA_ARGS__);	\
+	} while (0)
+
 #endif // _PAPERTRAIL_H
