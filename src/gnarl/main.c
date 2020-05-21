cce3d24db2213d42e490335cdb40e1992581775f
Jan Dittmer <jdi@l4x.org>
2020-05-21 21:54:08 +0200

    Add task watchdog
    This makes sure command processing is making progress.
    If for whatever reason we do not get a command for 5
    minutes, reboot.  This catches the other random hangs in
    the app which are now quite rare, but better than actually
    having to hardware reset the device, especially if it is
    connected to a battery in a case.

--- src/gnarl/main.c
+++ src/gnarl/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#include <esp_wifi.h>
+#include <esp_task_wdt.h>
+
 #include "display.h"
 #include "gnarl.h"
 #include "rfm95.h"
@@ -7,6 +10,8 @@
 #define PUMP_FREQUENCY 916600000
 
 void app_main(void) {
+	ESP_LOGD(TAG, "enable watchdog with a timeout of %d seconds", WDT_TIMEOUT_SECONDS);
+	esp_task_wdt_init(WDT_TIMEOUT_SECONDS, true);
 	ESP_LOGI(TAG, "%s", SUBG_RFSPY_VERSION);
 	rfm95_init();
 	uint8_t v = read_version();
