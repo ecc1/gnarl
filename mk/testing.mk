4192e6db5eec4f346c5220e99306a46b04ee1b65
Eric Cooper <ecc@cmu.edu>
2020-07-20 22:13:41 -0400

    Add include directories for tests

--- mk/testing.mk
+++ mk/testing.mk
@@ -15,8 +15,10 @@ clean:
 	rm -f *.o $(programs)
 
 TEST_DIR = ../../../test
+INC_DIRS = $(TEST_DIR) . .. ../../../include /usr/include/cjson
+INC_FLAGS = $(foreach dir,$(INC_DIRS),-I $(dir))
 
-CFLAGS += -Wall -Wempty-body -Wpedantic -Wunused -I . -I .. -I $(TEST_DIR) -I /usr/include/cjson
+CFLAGS += -Wall -Wempty-body -Wpedantic -Wunused $(INC_FLAGS)
 LDFLAGS += -lcjson -lm
 
 COMMON_CODE = $(TEST_DIR)/common.c
